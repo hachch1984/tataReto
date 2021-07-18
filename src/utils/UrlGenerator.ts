291c557e9d1e4c12c4dfdd97fe38cde340211436
Henry Alberto Chavez Chavez <hachch1984@gmail.com>
2021-07-17 20:18:44 -0500

    avance 2

--- src/utils/UrlGenerator.ts
+++ src/utils/UrlGenerator.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
 export const UrlGenerator = (urlName: string) => {
- // return "/" + urlName + "-" + uuidv4();
-  return '/'+urlName;
+   return "/" + urlName + "-" + uuidv4();
+  //return '/'+urlName;
 };
